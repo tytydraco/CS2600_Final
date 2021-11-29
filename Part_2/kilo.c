7a84bc651fe9b6f5521e92bb72fb0c0e00b4e969
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 14:46:37 -0800

    Restore terminal on exit
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -1,8 +1,18 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-	struct termios raw;
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
 
 	tcgetattr(STDIN_FILENO, &raw);
 	raw.c_lflag &= ~(ECHO);
