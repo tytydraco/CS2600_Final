b9bb0d29bbad179fc4c9c703c73b05b7de098df4
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 14:56:31 -0800

    Display individual keypresses
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #define TERM_FLAGS ~(ECHO | ICANON)
 
@@ -25,6 +27,14 @@ int main() {
 	enableRawMode();
 
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
+
 	return 0;
 }
