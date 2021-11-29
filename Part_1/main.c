fe486177e85640602d9a2643d6752b726e7cddc2
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 14:10:41 -0800

    Greatly simplify read line
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_1/main.c
+++ Part_1/main.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void)
 {
-  int bufsize = LSH_RL_BUFSIZE;
-  int position = 0;
-  char *buffer = malloc(sizeof(char) * bufsize);
-  int c;
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
 
-  if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
-
-  while (1) {
-    // Read a character
-    c = getchar();
-
-    // If we hit EOF, replace it with a null character and return.
-    if (c == EOF || c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
-
-    // If we have exceeded the buffer, reallocate.
-    if (position >= bufsize) {
-      bufsize += LSH_RL_BUFSIZE;
-      buffer = realloc(buffer, bufsize);
-      if (!buffer) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
+  if (getline(&line, &bufsize, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We recieved an EOF
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
     }
   }
+
+  return line;
 }
 
 void lsh_loop(void)
