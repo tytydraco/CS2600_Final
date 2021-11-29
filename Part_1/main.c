b0dcf6994c06d8f533aa400c36dfff8e6a3d121e
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 14:12:32 -0800

    Finally, a way to launch our shell
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_1/main.c
+++ Part_1/main.c
@@ -27,6 +27,24 @@ int lsh_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
+int lsh_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
+}
+
 /*
   Builtin function implementations.
 */
