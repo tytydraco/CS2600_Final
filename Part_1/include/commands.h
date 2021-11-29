6bd3efbfe70ce7f1bc17c3e30a3b2b5112ce3d8d
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 14:17:47 -0800

    Preprocessor inline number of builtins
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ Part_1/include/commands.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/*
+  Function Declarations for builtin shell commands:
+ */
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+#define lsh_num_builtins() sizeof(builtin_str) / sizeof(char *)
