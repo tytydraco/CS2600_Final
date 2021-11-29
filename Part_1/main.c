2daa2a341ce9355e47bf0feac150d587ab8ff0a7
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 14:11:10 -0800

    Method to separate lines and parse them
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_1/main.c
+++ Part_1/main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+
 char *lsh_read_line(void)
 {
   char *line = NULL;
