ae1484c214432a77b4ffe0e99922cdb6d8d2c1a9
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 14:17:04 -0800

    Move comands to include file
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_1/src/main.c
+++ Part_1/src/main.c
@@ -4,31 +4,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-/*
-  Function Declarations for builtin shell commands:
- */
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
-
-/*
-  List of builtin commands, followed by their corresponding functions.
- */
-char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
-};
-
-int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
-};
-
-int lsh_num_builtins() {
-  return sizeof(builtin_str) / sizeof(char *);
-}
+#include "../include/commands.h"
 
 int lsh_launch(char **args)
 {
