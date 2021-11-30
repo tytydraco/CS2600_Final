66b8c2d11800b40ee860ac89183a2c302be21413
Tyler Nijmeh <tylernij@gmail.com>
2021-11-30 15:08:04 -0800

    Highlight strings too
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -42,11 +42,13 @@ enum editorKey
 enum editorHighlight
 {
 	HL_NORMAL = 0,
+	HL_STRING,
 	HL_NUMBER,
 	HL_MATCH
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1 << 0)
+#define HL_HIGHLIGHT_STRINGS (1 << 1)
 
 #define TERM_FLAGS_I ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON)
 #define TERM_FLAGS_O ~(OPOST)
@@ -97,7 +99,7 @@ char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 struct editorSyntax HLDB[] = {
 	{"c",
 	 C_HL_extensions,
-	 HL_HIGHLIGHT_NUMBERS},
+	 HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS},
 };
 
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
@@ -280,11 +282,43 @@ void editorUpdateSyntax(erow *row)
 		return;
 
 	int prev_sep = 1;
+	int in_string = 0;
+
 	int i = 0;
 	while (i < row->rsize)
 	{
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+		if (E.syntax->flags & HL_HIGHLIGHT_STRINGS)
+		{
+			if (in_string)
+			{
+				row->hl[i] = HL_STRING;
+				if (c == '\\' && i + 1 < row->rsize)
+				{
+					row->hl[i + 1] = HL_STRING;
+					i += 2;
+					continue;
+				}
+				if (c == in_string)
+					in_string = 0;
+				i++;
+				prev_sep = 1;
+				continue;
+			}
+			else
+			{
+				if (c == '"' || c == '\'')
+				{
+					in_string = c;
+					row->hl[i] = HL_STRING;
+					i++;
+					continue;
+				}
+			}
+		}
+
 		if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS)
 		{
 			if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
@@ -305,6 +339,8 @@ int editorSyntaxToColor(int hl)
 {
 	switch (hl)
 	{
+	case HL_STRING:
+		return 35;
 	case HL_NUMBER:
 		return 31;
 	case HL_MATCH:
