0bd7b11c521771378aeb3460c0f37749e80b96bc
Tyler Nijmeh <tylernij@gmail.com>
2021-11-30 14:40:48 -0800

    Save
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -15,6 +15,7 @@
 #include <sys/types.h>
 #include <time.h>
 #include <stdarg.h>
+#include <fcntl.h>
 
 /*** defines ***/
 
@@ -69,6 +70,10 @@ struct editorConfig
 };
 
 struct editorConfig E;
+
+/*** prototypes ***/
+
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** terminal ***/
 
@@ -307,6 +312,25 @@ void editorInsertChar(int c)
 
 /*** file i/o ***/
 
+char *editorRowsToString(int *buflen)
+{
+	int totlen = 0;
+	int j;
+	for (j = 0; j < E.numrows; j++)
+		totlen += E.row[j].size + 1;
+	*buflen = totlen;
+	char *buf = malloc(totlen);
+	char *p = buf;
+	for (j = 0; j < E.numrows; j++)
+	{
+		memcpy(p, E.row[j].chars, E.row[j].size);
+		p += E.row[j].size;
+		*p = '\n';
+		p++;
+	}
+	return buf;
+}
+
 void editorOpen(char *filename)
 {
 	free(E.filename);
@@ -330,7 +354,33 @@ void editorOpen(char *filename)
 	fclose(fp);
 }
 
+void editorSave()
+{
+	if (E.filename == NULL)
+		return;
+	int len;
+	char *buf = editorRowsToString(&len);
+	int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+	if (fd != -1)
+	{
+		if (ftruncate(fd, len) != -1)
+		{
+			if (write(fd, buf, len) == len)
+			{
+				close(fd);
+				free(buf);
+				editorSetStatusMessage("%d bytes written to disk", len);
+				return;
+			}
+		}
+		close(fd);
+	}
+	free(buf);
+	editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
 /*** append buffer ***/
+
 struct abuf
 {
 	char *b;
@@ -559,6 +609,10 @@ void editorProcessKeypress()
 		exit(0);
 		break;
 
+	case CTRL_KEY('s'):
+		editorSave();
+		break;
+
 	case HOME_KEY:
 		E.cx = 0;
 		break;
@@ -639,7 +693,7 @@ int main(int argc, char *argv[])
 		editorOpen(argv[1]);
 	}
 
-	editorSetStatusMessage("HELP: Ctrl-Q = quit");
+	editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
 	while (1)
 	{
