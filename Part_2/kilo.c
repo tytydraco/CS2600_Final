d03b1c4c508da9bbcb2ce73f2a3d656371d8d4c2
Tyler Nijmeh <tylernij@gmail.com>
2021-11-30 14:22:20 -0800

    Horizontal scrolling
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -50,6 +50,7 @@ struct editorConfig
 {
 	int cx, cy;
 	int rowoff;
+	int coloff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -288,6 +289,14 @@ void editorScroll()
 	{
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	if (E.cx < E.coloff)
+	{
+		E.coloff = E.cx;
+	}
+	if (E.cx >= E.coloff + E.screencols)
+	{
+		E.coloff = E.cx - E.screencols + 1;
+	}
 }
 
 void editorDrawRows(struct abuf *ab)
@@ -322,10 +331,12 @@ void editorDrawRows(struct abuf *ab)
 		}
 		else
 		{
-			int len = E.row[filerow].size;
+			int len = E.row[filerow].size - E.coloff;
+			if (len < 0)
+				len = 0;
 			if (len > E.screencols)
 				len = E.screencols;
-			abAppend(ab, E.row[filerow].chars, len);
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
 		}
 		abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows - 1)
@@ -344,7 +355,8 @@ void editorRefreshScreen()
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+			 (E.cx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	abAppend(&ab, "\x1b[H", 3);
@@ -364,8 +376,7 @@ void editorMoveCursor(int key)
 			E.cx--;
 		break;
 	case ARROW_RIGHT:
-		if (E.cx != E.screencols - 1)
-			E.cx++;
+		E.cx++;
 		break;
 	case ARROW_UP:
 		if (E.cy != 0)
@@ -421,6 +432,7 @@ void initEditor()
 	E.cx = 0;
 	E.cy = 0;
 	E.rowoff = 0;
+	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 
