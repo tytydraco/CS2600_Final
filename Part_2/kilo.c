9e50a4e03d4b96a46dc1ee426767b3e04049dfdf
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 19:02:57 -0800

    Catch Ctrl-V
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <ctype.h>
 
-#define TERM_FLAGS ~(IXON | ECHO | ICANON | ISIG)
+#define TERM_FLAGS ~(IXON | ECHO | ICANON | IEXTEN | ISIG)
 
 struct termios orig_termios;
 
