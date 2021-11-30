3fecc384bb9c0231e21d4abe032ff9ec0080766e
Tyler Nijmeh <tylernij@gmail.com>
2021-11-30 15:02:33 -0800

    Highlight search results
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -42,7 +42,8 @@ enum editorKey
 enum editorHighlight
 {
 	HL_NORMAL = 0,
-	HL_NUMBER
+	HL_NUMBER,
+	HL_MATCH
 };
 
 #define TERM_FLAGS_I ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON)
@@ -264,6 +265,8 @@ int editorSyntaxToColor(int hl)
 	{
 	case HL_NUMBER:
 		return 31;
+	case HL_MATCH:
+		return 34;
 	default:
 		return 37;
 	}
@@ -530,6 +533,16 @@ void editorFindCallback(char *query, int key)
 {
 	static int last_match = -1;
 	static int direction = 1;
+
+	static int saved_hl_line;
+	static char *saved_hl = NULL;
+	if (saved_hl)
+	{
+		memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+		free(saved_hl);
+		saved_hl = NULL;
+	}
+
 	if (key == '\r' || key == '\x1b')
 	{
 		last_match = -1;
@@ -568,6 +581,11 @@ void editorFindCallback(char *query, int key)
 			E.cy = current;
 			E.cx = editorRowRxToCx(row, match - row->render);
 			E.rowoff = E.numrows;
+
+			saved_hl_line = current;
+			saved_hl = malloc(row->rsize);
+			memcpy(saved_hl, row->hl, row->rsize);
+			memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
 			break;
 		}
 	}
