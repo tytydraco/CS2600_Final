0d5a593ecf3b9a9218ba0fdc52c20ab478cec88d
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 19:34:24 -0800

    Clear lines individually/
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -122,6 +122,7 @@ void editorDrawRows(struct abuf *ab) {
 	int y;
 	for (y = 0; y < E.screenrows; y++) {
 		abAppend(ab, "~", 1);
+		abAppend(ab, "\x1b[K", 3);
 		write(STDOUT_FILENO, "~", 1);
 		if (y < E.screenrows - 1) {
 			abAppend(ab, "\r\n", 2);
@@ -131,10 +132,11 @@ void editorDrawRows(struct abuf *ab) {
 
 void editorRefreshScreen() {
 	struct abuf ab = ABUF_INIT;
-	abAppend(&ab, "\x1b[2J", 4);
+	abAppend(&ab, "\x1b[?25l", 6);
 	abAppend(&ab, "\x1b[H", 3);
 	editorDrawRows(&ab);
 	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6);
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
 }
