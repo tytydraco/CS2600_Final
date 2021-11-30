72167382f46170b203d7941bedfb467f8952b54a
Tyler Nijmeh <tylernij@gmail.com>
2021-11-30 14:38:56 -0800

    No special characters allowed!
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -25,6 +25,7 @@
 
 enum editorKey
 {
+	BACKSPACE = 127,
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_UP,
@@ -548,6 +549,10 @@ void editorProcessKeypress()
 	int c = editorReadKey();
 	switch (c)
 	{
+	case '\r':
+		/* TODO */
+		break;
+
 	case CTRL_KEY('q'):
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
@@ -560,6 +565,12 @@ void editorProcessKeypress()
 	case END_KEY:
 		if (E.cy < E.numrows)
 			E.cx = E.row[E.cy].size;
+		break;
+
+	case BACKSPACE:
+	case CTRL_KEY('h'):
+	case DEL_KEY:
+		/* TODO */
 		break;
 
 	case PAGE_UP:
@@ -588,6 +599,11 @@ void editorProcessKeypress()
 	case ARROW_RIGHT:
 		editorMoveCursor(c);
 		break;
+
+	case CTRL_KEY('l'):
+	case '\x1b':
+		break;
+
 	default:
 		editorInsertChar(c);
 		break;
