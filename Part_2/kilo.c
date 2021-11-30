55ff4df74e69bd4e5e5311c39f00e70fb5d7f986
Tyler Nijmeh <tylernij@gmail.com>
2021-11-30 14:52:38 -0800

    Find callback
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -77,7 +77,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** terminal ***/
 
@@ -457,7 +457,7 @@ void editorSave()
 {
 	if (E.filename == NULL)
 	{
-		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
 		if (E.filename == NULL)
 		{
 			editorSetStatusMessage("Save aborted");
@@ -488,11 +488,12 @@ void editorSave()
 
 /*** find ***/
 
-void editorFind()
+void editorFindCallback(char *query, int key)
 {
-	char *query = editorPrompt("Search: %s (ESC to cancel)");
-	if (query == NULL)
+	if (key == '\r' || key == '\x1b')
+	{
 		return;
+	}
 	int i;
 	for (i = 0; i < E.numrows; i++)
 	{
@@ -506,7 +507,14 @@ void editorFind()
 			break;
 		}
 	}
-	free(query);
+}
+void editorFind()
+{
+	char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+	if (query)
+	{
+		free(query);
+	}
 }
 
 /*** append buffer ***/
@@ -678,7 +686,8 @@ void editorSetStatusMessage(const char *fmt, ...)
 }
 
 /*** input ***/
-char *editorPrompt(char *prompt)
+
+char *editorPrompt(char *prompt, void (*callback)(char *, int))
 {
 	size_t bufsize = 128;
 	char *buf = malloc(bufsize);
@@ -697,6 +706,8 @@ char *editorPrompt(char *prompt)
 		else if (c == '\x1b')
 		{
 			editorSetStatusMessage("");
+			if (callback)
+				callback(buf, c);
 			free(buf);
 			return NULL;
 		}
@@ -705,6 +716,8 @@ char *editorPrompt(char *prompt)
 			if (buflen != 0)
 			{
 				editorSetStatusMessage("");
+				if (callback)
+					callback(buf, c);
 				return buf;
 			}
 		}
@@ -718,6 +731,8 @@ char *editorPrompt(char *prompt)
 			buf[buflen++] = c;
 			buf[buflen] = '\0';
 		}
+		if (callback)
+			callback(buf, c);
 	}
 }
 
