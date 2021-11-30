946ead97a9feb42b8c524f6fb01dd37e69070f14
Tyler Nijmeh <tylernij@gmail.com>
2021-11-29 19:12:23 -0800

    Add a timeout for read
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -26,20 +26,23 @@ void enableRawMode() {
 	raw.c_oflag &= TERM_FLAGS_O;
 	raw.c_cflag |= TERM_FLAGS_C;
 	raw.c_lflag &= TERM_FLAGS_L;
+	raw.c_cc[VMIN] = 0;
+  	raw.c_cc[VTIME] = 1;
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
 	enableRawMode();
 
-	char c;
-
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	while (1) {
+		char c = '\0';
+		read(STDIN_FILENO, &c, 1);
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		} else {
 			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == 'q') break;
 	}
 
 	return 0;
