8d468f5172d09d73bbc942676ef5cc22421d1456
Tyler Nijmeh <tylernij@gmail.com>
2021-11-30 14:43:05 -0800

    are you really really sure?
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -21,6 +21,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k)&0x1f)
 
@@ -602,6 +603,8 @@ void editorMoveCursor(int key)
 
 void editorProcessKeypress()
 {
+	static int quit_times = KILO_QUIT_TIMES;
+
 	int c = editorReadKey();
 	switch (c)
 	{
@@ -610,6 +613,14 @@ void editorProcessKeypress()
 		break;
 
 	case CTRL_KEY('q'):
+		if (E.dirty && quit_times > 0)
+		{
+			editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+								   "Press Ctrl-Q %d more times to quit.",
+								   quit_times);
+			quit_times--;
+			return;
+		}
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
@@ -668,6 +679,8 @@ void editorProcessKeypress()
 		editorInsertChar(c);
 		break;
 	}
+
+	quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
