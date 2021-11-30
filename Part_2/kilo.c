adcdf9d00b9273b4344fbbb643ab54895f47161f
Tyler Nijmeh <tylernij@gmail.com>
2021-11-30 14:36:20 -0800

    Status message
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- Part_2/kilo.c
+++ Part_2/kilo.c
@@ -13,6 +13,8 @@
 #include <sys/ioctl.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
+#include <stdarg.h>
 
 /*** defines ***/
 
@@ -60,6 +62,8 @@ struct editorConfig
 	int numrows;
 	erow *row;
 	char *filename;
+	char statusmsg[80];
+	time_t statusmsg_time;
 	struct termios orig_termios;
 };
 
@@ -425,6 +429,17 @@ void editorDrawStatusBar(struct abuf *ab)
 		}
 	}
 	abAppend(ab, "\x1b[m", 3);
+	abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(struct abuf *ab)
+{
+	abAppend(ab, "\x1b[K", 3);
+	int msglen = strlen(E.statusmsg);
+	if (msglen > E.screencols)
+		msglen = E.screencols;
+	if (msglen && time(NULL) - E.statusmsg_time < 5)
+		abAppend(ab, E.statusmsg, msglen);
 }
 
 void editorRefreshScreen()
@@ -435,6 +450,7 @@ void editorRefreshScreen()
 	abAppend(&ab, "\x1b[H", 3);
 	editorDrawRows(&ab);
 	editorDrawStatusBar(&ab);
+	editorDrawMessageBar(&ab);
 
 	char buf[32];
 	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
@@ -445,6 +461,15 @@ void editorRefreshScreen()
 	abAppend(&ab, "\x1b[?25h", 6);
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
+}
+
+void editorSetStatusMessage(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+	va_end(ap);
+	E.statusmsg_time = time(NULL);
 }
 
 /*** input ***/
@@ -555,10 +580,12 @@ void initEditor()
 	E.numrows = 0;
 	E.row = NULL;
 	E.filename = NULL;
+	E.statusmsg[0] = '\0';
+	E.statusmsg_time = 0;
 
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
 		die("getWindowSize");
-	E.screenrows -= 1;
+	E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[])
@@ -569,6 +596,8 @@ int main(int argc, char *argv[])
 	{
 		editorOpen(argv[1]);
 	}
+
+	editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
 	while (1)
 	{
